d15a3cb5540d0ca5e5215a7093fe36f4f51213f0
Jeremy Whitaker <github@jwhitaker.info>
2014-07-20 16:48:12 -0700

    Added data to band and musician objects. Data looks correct in Chrome console but arrayed objects are not passing chai yet.

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ var Musician = {
 }
 
 var Band = {
-  initialize: function(bandName, bandGenre, bandMembers){
+  initialize: function(bandName, bandGenre, Musician){
     this.bandName = bandName;
     this.bandGenre = bandGenre;
     this.bandMembers = [];
@@ -16,5 +16,65 @@ var Band = {
   }
 }
 
-// var sexPistols = Object.create(Band);
-// sexPistols.initialize("The Sex Pistols", "Punk Rock",["John Lydon", "Sid Vicious"]);
+//SexPistols members initialization
+var johnnyRotten = Object.create(Musician);
+johnnyRotten.initialize("Johnny Rotten", "Vocals");
+
+var sidVicious = Object.create(Musician);
+sidVicious.initialize("Sid Vicious", "Bass Guitar");
+
+var steveJones = Object.create(Musician);
+steveJones.initialize("Steve Jones", "Guitar");
+
+var paulCook = Object.create(Musician);
+paulCook.initialize("Paul Cook", "Drums");
+
+//Beatles members initialization
+var johnLennon = Object.create(Musician);
+johnLennon.initialize("John Lennon", "Vocals");
+
+var paulMccartney = Object.create(Musician);
+paulMccartney.initialize("Paul McCartney", "Bass Guitar");
+
+var georgeHarrison = Object.create(Musician);
+georgeHarrison.initialize("George Harrison", "Guitar");
+
+var ringoStarr = Object.create(Musician);
+ringoStarr.initialize("Ringo Starr", "Drums");
+
+//Queen musicians initialization
+var freddieMercury = Object.create(Musician);
+freddieMercury.initialize('Freddie Mercury', 'Vocals');
+
+var brianMay = Object.create(Musician);
+brianMay.initialize('Brian May','Guitar');
+
+var johnDeacon = Object.create(Musician);
+johnDeacon.initialize('John Deacon','Bass Guitar');
+
+var rogerTaylor = Object.create(Musician);
+rogerTaylor.initialize('Roger Taylor','Drums');
+
+// Band initiailzation
+var sexPistols = Object.create(Band);
+sexPistols.initialize("The Sex Pistols", "Punk Rock");
+sexPistols.bandMembers.push(johnnyRotten, sidVicious, steveJones, paulCook);
+
+var beatles = Object.create(Band);
+beatles.initialize("The Beatles", "Brit Pop");
+beatles.bandMembers.push(johnLennon, paulMccartney, georgeHarrison, ringoStarr);
+
+var queen = Object.create(Band);
+queen.initialize("Queen", "Glam Rock");
+queen.bandMembers.push(freddieMercury, brianMay, johnDeacon, rogerTaylor);
+
+
+
+
+
+console.log(sexPistols);
+console.log(beatles);
+console.log(queen);
+
+
+
--- spec/specs.js
+++ spec/specs.js
@@ -10,26 +10,33 @@ describe("Musician", function() {
 });
 describe("Band", function() {
   describe("initialize", function() {
-    it("build an band object containing core band values(name/genre)", function() {
+    it("build a band object containing core band values(name/genre)", function() {
+
+      var newMusician = Object.create(Musician);
       var newBand = Object.create(Band);
-      // var newMusician = Object.create(Musician);
+
+      johnnyRotten.initialize("Johnny Rotten", "Vocals");
+      sidVicious.initialize("Sid Vicious", "Bass Guitar");
 
       newBand.initialize("The Sex Pistols", "Punk Rock");
+      newBand.bandMembers.push(johnnyRotten, sidVicious);
+      
       newBand.bandName.should.equal("The Sex Pistols");
       newBand.bandGenre.should.equal("Punk Rock");
-      newBand.bandMembers.should.eql([])
       
-
-      // newMusician.initialize("Sid Vicious", "Guitar");
-      // newMusician.memberName.should.equal("Sid Vicious");
-      // //newMusician.instrument.should.equal("Guitar");
-
-      // newMusician.initialize("John Lydon", "Vocal");
-      // newMusician.memberName.should.equal("John Lydon");
-      // //newMusician.instrument.should.equal("Vocals");
+      newBand.bandMembers.should.eql(
+        [{instrument: "Bass Guitar", memberName: "Sid Vicious"},{instrument: "Vocals", memberName: "Johnny Rotten"}]);
 
     });
   });
+  // describe("initialize", function() {
+  //   it("build an band object containing core band values(name/genre)", function() {
+  //     var newBand = Object.create(Band);
+  //     // var newMusician = Object.create(Musician);
 
+  //     newBand.initialize("The Sex Pistols", "Punk Rock");
+  //     newBand.bandName.should.equal("The Sex Pistols");
+  //     newBand.bandGenre.should.equal("Punk Rock");
+  //     newBand.bandMembers.should.eql([])
 
 });
